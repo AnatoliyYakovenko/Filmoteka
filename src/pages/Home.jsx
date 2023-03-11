231be0107a79d1cff0ec8daed51800a7fcbb09f2
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-03-11 13:09:45 +0200

    add pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+import { MovieList } from 'components/MovieList/MovieList';
+import { useState, useEffect } from 'react';
+import { getTrendingMovie } from 'services/movieAPI';
+import css from './Home.module.css';
+import Loader from "components/Loader/Loader";
+
+export const Home = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    getTrendingMovie().then(({ results }) => {
+      setMovies(results);
+    });
+  }, []);
+  return (
+    <main>
+      <section className={css.gallery}>
+        <div className={css.container}>
+          <h1 className={css.title}>Tranding today</h1>
+          <MovieList movies={movies} />
+        </div>
+      </section>
+      <Suspense fallback={<Loader/>}>
+        <Outlet />
+      </Suspense>
+    </main>
+
+  );
+};
